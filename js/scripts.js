566529b097c01af6ab8707c4790801ed640a71d8
Brenda Franco and Matt Miller <matt@>
2018-03-22 11:58:12 -0700

    clears the returned id from prior submitt

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+
+//This clears the form
+    $("#turtle").hide();
+    $("#snake").hide();
+    $("#insect").hide();
+
     var formInput = $("#animal").val();
 
     if (formInput === "Turtle") {
       $("#turtle").show();
-      $("#snake").hide();
-      $("#insect").hide();
     } else if (formInput === "Snake") {
       $("#snake").show();
-      $("#turtle").hide();
-      $("#insect").hide();
     } else {
       $("#insect").show();
-      $("#turtle").hide();
-      $("#snake").hide();
     }
 
   });
